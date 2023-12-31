a1918912652950e706b7da9f6eaaedf34ad4ca14
jaguhdeveloper@gmail.com <geraldwainaina25@gmail.com>
2023-12-31 22:42:27 +0300

    hello

---
+++ app/src/main/java/com/little_teny_v1/room/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.little_teny_v1.room
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.little_teny_v1.room.data.NotesDatabase
+import com.little_teny_v1.room.presentation.AddNoteScreen
+import com.little_teny_v1.room.presentation.NotesScreen
+import com.little_teny_v1.room.presentation.NotesViewModel
+import com.little_teny_v1.room.ui.theme.RoomTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val database by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            NotesDatabase::class.java,
+            "notes.db"
+        ).build()
+    }
+
+    private val viewModel: NotesViewModel by viewModels<NotesViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory{
+                override fun<T: ViewModel> create(modelClass: Class<T>): T {
+                    return NotesViewModel(database.dao) as T
+                }
+            }
+        }
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RoomTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    val state by viewModel.state.collectAsState()
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "NotesScreen") {
+                        composable("NotesScreen") {
+                            NotesScreen(
+                                state = state,
+                                navController = navController,
+                                onEvent = viewModel::onEvent
+                            )
+                        }
+                        composable("AddNoteScreen") {
+                            AddNoteScreen(
+                                state = state,
+                                navController = navController,
+                                onEvent = viewModel::onEvent
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
